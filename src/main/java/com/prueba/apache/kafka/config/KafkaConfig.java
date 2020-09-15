42b69a6cb0779c2f2f33d61ee92820109f2d5476
sonia.cabrera <sonia.cabrera@mh.gob.sv>
2020-09-14 18:15:20 -0600

    4

--- src/main/java/com/prueba/apache/kafka/config/KafkaConfig.java
+++ src/main/java/com/prueba/apache/kafka/config/KafkaConfig.java
@@ -2,15 +2,21 @@ package com.prueba.apache.kafka.config;
 
 import com.prueba.apache.kafka.mensajeDTO.ResultMsj;
 import com.prueba.apache.kafka.mensajeDTO.VehiculoMsj;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 /**
  *
@@ -18,6 +24,27 @@ import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
  */
 @Configuration
 public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, VehiculoMsj> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+// Standard KafkaProducer settings - specifying brokerand serializer 
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> configProps = new HashMap<>();
+         configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://172.17.0.8:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class);
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+       
+        return configProps;
+    }
 
     @Bean
     public ReplyingKafkaTemplate<String, VehiculoMsj, ResultMsj> replyingKafkaTemplate(ProducerFactory<String, VehiculoMsj> pf,
